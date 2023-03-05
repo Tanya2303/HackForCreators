b5eae63e6f7c5f6469c146aea69f2d5d2b0b8e17
Tanya Raikwar <115107614+Tanya2303@users.noreply.github.com>
2023-03-05 16:17:41 +0530

    Add context

--- website/client/src/pages/Explore.jsx
+++ website/client/src/pages/Explore.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 export default function Explore() {
   return (
--- website/client/src/pages/MyNFTs.jsx
+++ website/client/src/pages/MyNFTs.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 export default function MyNFTs() {
   return (
